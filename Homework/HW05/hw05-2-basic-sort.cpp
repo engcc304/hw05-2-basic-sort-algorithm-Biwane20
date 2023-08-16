a0f225cd8b4c3d9cdb57e0bcf4e48b807e4133ae
Biwane20 <biwbew20@gmail.com>
2023-08-16 23:38:55 +0700

    HW5.2

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -1,6 +1,6 @@
 /*
     จงเรียงลำดับเลขจากข้อมูลที่ผู้ใช้กรอก โดยเรียงจากมากไปน้อย (โดยใช้คำสั่ง if else หรือ else if เท่านั้น)
-    
+
     Test case:
         Input[1] :
             6
@@ -21,3 +21,43 @@
     Output:
         112 91 65
 */
+#include <stdio.h>
+int main()
+{
+    int a = 0, b = 0, c = 0;
+    printf("Input[1] ");
+    scanf("%d", &a);
+    printf("Input[2] ");
+    scanf("%d", &b);
+    printf("Input[3] ");
+    scanf("%d", &c);
+
+    if (a > b && a > c)
+    {
+        printf("%d ", a);
+        if (b > c)
+            printf("%d %d ", b, c);
+        else
+            printf("%d %d ", c, b);
+    }
+
+    else if (b > a && b > c)
+    {
+        printf("%d ", b);
+        if (a > c)
+            printf("%d %d ", a, c);
+        else
+            printf("%d %d ", c, a);
+    }
+
+    else if (c > b && c > a)
+    {
+        printf("%d ", c);
+        if (b > a)
+            printf("%d %d ", b, a);
+        else
+            printf("%d %d ", a, b);
+    }
+
+    return 0;
+}
